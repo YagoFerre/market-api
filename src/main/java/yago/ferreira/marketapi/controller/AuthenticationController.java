588766409ef21804433f7057c785bddd734ad3e8
yagof <yagofer12@gmail.com>
2025-01-05 15:46:42 -0300

    Auth infra

---
+++ src/main/java/yago/ferreira/marketapi/controller/AuthenticationController.java
@@ -0,0 +1,57 @@
+package yago.ferreira.marketapi.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import yago.ferreira.marketapi.entity.Usuario;
+import yago.ferreira.marketapi.entity.dto.AuthenticationDTO;
+import yago.ferreira.marketapi.entity.dto.RegisterDTO;
+import yago.ferreira.marketapi.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthenticationController {
+
+    private final UsuarioRepository usuarioRepository;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationController(UsuarioRepository usuarioRepository, AuthenticationManager authenticationManager) {
+        this.usuarioRepository = usuarioRepository;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity createUser(@RequestBody @Valid RegisterDTO registerDTO) {
+        UserDetails usuarioExistente = usuarioRepository.findByEmail(registerDTO.getEmail());
+
+        if (usuarioExistente != null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        String senhaEncrypted = new BCryptPasswordEncoder().encode(registerDTO.getSenha());
+
+        Usuario novoUsuario = new Usuario();
+        novoUsuario.setEmail(registerDTO.getEmail());
+        novoUsuario.setNome(registerDTO.getNome());
+        novoUsuario.setSenha(senhaEncrypted);
+
+        usuarioRepository.save(novoUsuario);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO dto) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getSenha());
+        var auth = authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
--- src/main/java/yago/ferreira/marketapi/repository/UsuarioRepository.java
+++ src/main/java/yago/ferreira/marketapi/repository/UsuarioRepository.java
@@ -1,8 +1,9 @@
 package yago.ferreira.marketapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import yago.ferreira.marketapi.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    UserDetails findByEmail(String email);
 }
