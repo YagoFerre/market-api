811a353c8b60662c9def9e7704880469f8a157fc
yagof <yagofer12@gmail.com>
2025-01-18 21:21:48 -0300

    Clean ProdutoMapper code

--- src/main/java/yago/ferreira/marketapi/entity/mapper/ProdutoMapper.java
+++ src/main/java/yago/ferreira/marketapi/entity/mapper/ProdutoMapper.java
@@ -1,39 +1,125 @@
 package yago.ferreira.marketapi.entity.mapper;
 
 import org.springframework.stereotype.Component;
+import yago.ferreira.marketapi.entity.File;
 import yago.ferreira.marketapi.entity.Produto;
+import yago.ferreira.marketapi.entity.Usuario;
 import yago.ferreira.marketapi.entity.dto.FileDTO;
 import yago.ferreira.marketapi.entity.dto.ProdutoDTO;
 import yago.ferreira.marketapi.entity.dto.UsuarioDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class ProdutoMapper {
 
     public ProdutoDTO toDTO(Produto produto) {
-        UsuarioDTO usuarioDTO = new UsuarioDTO(
-                produto.getUsuario().getNome(),
-                produto.getUsuario().getEmail()
-        );
+        if (produto == null) {
+            return null;
+        }
 
-        List<FileDTO> filesDTO = produto.getProdutoImagem()
-                .stream()
-                .map(file -> new FileDTO(file.getId(), file.getNome(), file.getFilePath()))
-                .toList();
+        ProdutoDTO produtoDTO = new ProdutoDTO();
 
-        return new ProdutoDTO(
-                produto.getId(),
-                produto.getTitulo(),
-                produto.getDescricao(),
-                produto.getPreco(),
-                usuarioDTO,
-                filesDTO
-        );
+        produtoDTO.setId(produto.getId());
+        produtoDTO.setTitulo(produto.getTitulo());
+        produtoDTO.setDescricao(produto.getDescricao());
+        produtoDTO.setPreco(produto.getPreco());
+        produtoDTO.setUsuario(toUsuarioDTO(produto.getUsuario()));
+        produtoDTO.setProdutoImagem(toFileDTOList(produto.getProdutoImagem()));
+        return produtoDTO;
     }
 
     public Produto toEntity(ProdutoDTO produtoDTO) {
-        return new Produto();
+        if (produtoDTO == null) {
+            return null;
+        }
+
+        Produto produto = new Produto();
+
+        produto.setId(produtoDTO.getId());
+        produto.setTitulo(produtoDTO.getTitulo());
+        produto.setDescricao(produtoDTO.getDescricao());
+        produto.setPreco(produtoDTO.getPreco());
+        produto.setUsuario(toUsuarioEntity(produtoDTO.getUsuario()));
+        produto.setProdutoImagem(toFileEntityList(produtoDTO.getProdutoImagem()));
+        return produto;
+    }
+
+    private UsuarioDTO toUsuarioDTO(Usuario usuario) {
+        if (usuario == null) {
+            return null;
+        }
+
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setEmail(usuario.getEmail());
+        usuarioDTO.setNome(usuario.getNome());
+        return usuarioDTO;
+    }
+
+    private Usuario toUsuarioEntity(UsuarioDTO usuarioDTO) {
+        if (usuarioDTO == null) {
+            return null;
+        }
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail(usuarioDTO.getEmail());
+        usuario.setNome(usuarioDTO.getNome());
+        return usuario;
+    }
+
+    private FileDTO toFileDTO(File file) {
+        if (file == null) {
+            return null;
+        }
+
+        FileDTO fileDTO = new FileDTO();
+
+        fileDTO.setId(file.getId());
+        fileDTO.setNome(file.getNome());
+        fileDTO.setFilePath(file.getFilePath());
+        return fileDTO;
+    }
+
+    private File toFileEntity(FileDTO fileDTO) {
+        if (fileDTO == null) {
+            return null;
+        }
+
+        File file = new File();
+
+        file.setId(fileDTO.getId());
+        file.setNome(fileDTO.getNome());
+        file.setFilePath(fileDTO.getFilePath());
+        return file;
+    }
+
+    private List<FileDTO> toFileDTOList(List<File> files) {
+        if (files == null) {
+            return null;
+        }
+
+        List<FileDTO> filesDTO = new ArrayList<>();
+
+        for (File file : files) {
+            filesDTO.add(toFileDTO(file));
+        }
+
+        return filesDTO;
+    }
+
+    private List<File> toFileEntityList(List<FileDTO> filesDTO) {
+        if (filesDTO == null) {
+            return null;
+        }
+
+        List<File> files = new ArrayList<>();
+
+        for (FileDTO fileDTO : filesDTO) {
+            files.add(toFileEntity(fileDTO));
+        }
+
+        return files;
     }
 
 }
