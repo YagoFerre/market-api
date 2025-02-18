c9b48cd7bfaf83ed3c0bc5ffcef0d37da9e73177
yagof <yagofer12@gmail.com>
2025-02-17 22:10:52 -0300

    Fix Ioc SpringApplication

--- src/main/java/yago/ferreira/marketapi/MarketApiApplication.java
+++ src/main/java/yago/ferreira/marketapi/MarketApiApplication.java
@@ -3,17 +3,10 @@ package yago.ferreira.marketapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import yago.ferreira.marketapi.infra.configuration.storage.FileStorageProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(FileStorageProperties.class)
-@ComponentScan({
-        "yago.ferreira.marketapi.domain.port",
-        "yago.ferreira.marketapi.application.service",
-        "yago.ferreira.marketapi.adapters",
-        "yago.ferreira.marketapi.adapters.out.encoder"
-})
 public class MarketApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarketApiApplication.class, args);
--- src/main/java/yago/ferreira/marketapi/adapters/out/encoder/PasswordEncoderImpl.java
+++ src/main/java/yago/ferreira/marketapi/adapters/out/encoder/PasswordEncoderImpl.java
@@ -1,8 +1,10 @@
 package yago.ferreira.marketapi.adapters.out.encoder;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 import yago.ferreira.marketapi.domain.port.in.usecases.PasswordEncoderUseCases;
 
+@Component
 public class PasswordEncoderImpl implements PasswordEncoderUseCases {
     @Override
     public String encode(String password) {
